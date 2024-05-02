f3ce3c1fdf880c972d2447eb5ecea49a046ef4ee
Hanif Abdul Baqi <64836582+haneefSaeed@users.noreply.github.com>
2024-05-03 01:26:39 +0300

    hero

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -11,9 +11,7 @@ const Layout = ({children}: props)=>{
 
             <Header />
             <Hero />
-            <div className="container mx-auto py-10 flex-1">
-                {children}
-            </div>
+
             <Footer/>
         </div>
     )
