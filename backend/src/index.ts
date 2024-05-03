68affa134213cd71759112d50e88c223903671d0
Hanif Abdul Baqi <64836582+haneefSaeed@users.noreply.github.com>
2024-05-03 16:16:25 +0300

    ORIGIN URL

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    // origin: process.env.FRONTEND_URL,
     credentials:true,
 }));
 
