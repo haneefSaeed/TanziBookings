dbce6ce8a98e0e3a5bc85aefee67ae0c49496bce
Hanif Abdul Baqi <hanif.sayeedi@gmail.com>
2025-11-27 13:10:13 +0300

    port dynamic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -58,6 +58,8 @@ app.get("*", (req: Request, res: Response)=>{
      res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'))
 })
 
-app.listen(5000, ()=>{
-    console.log("server running at 5000")
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
